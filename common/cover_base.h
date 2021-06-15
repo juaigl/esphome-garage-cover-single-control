7eecd7ac77cfb773c338a8cb419a7e1164f4d3bb
Juan M <jmif@outlook.es>
2021-06-15 19:57:05 +0200

    Migrated cover code from yaml to cover_base.h

--- common/cover_base.h
+++ common/cover_base.h
@@ -11,6 +11,7 @@ enum Action : uint8_t
 class CustomGarageCover : public Component, public Cover
 {
 public:
+    // TODO add custom constructor
 
     void setup() override
     {
@@ -30,25 +31,59 @@ public:
     void control(const CoverCall &call) override
     {
         // This will be called every time the user requests a state change.
+
         if (call.get_position().has_value())
         {
+            // get requested position
             float pos = *call.get_position();
-            // Write pos (range 0-1) to cover
-            // ...
 
-            // Publish new state
-            this->position = pos;
-            this->publish_state();
+            if (pos != this->position)
+            {
+                // not at target
+                target_action = pos < this->position ? ACTION_CLOSE : ACTION_OPEN;
+            }
         }
         if (call.get_stop())
         {
-            // User requested cover stop
+            target_action = ACTION_IDLE;
         }
     }
 
     void loop() override
     {
         // This will be called by App.loop()
+
+        static uint32_t last_publish_time = 0;
+        static uint32_t last_push_time = 0;
+
+        // store current time
+        const uint32_t now = millis();
+
+        // Recompute position every loop cycle
+        this->recompute_position();
+
+        // push button if target action different than current operation
+        if (target_action != ACTION_NONE && target_action != this->current_operation)
+        {
+            if (now - last_push_time > push_interval)
+            {
+                this->push_button();
+                last_push_time = now;
+            }
+        }
+        else if (target_action == this->current_operation)
+        {
+            // target reached, set target as None (3).
+            ESP_LOGD("target", "Target Reached");
+            target_action = ACTION_NONE;
+        }
+
+        // Send current position every second
+        if (this->current_operation != COVER_OPERATION_IDLE && now - this->last_publish_time_ > 1000)
+        {
+            this->publish_state(false);
+            this->last_publish_time_ = now;
+        }
     }
 
 private:
@@ -60,11 +95,65 @@ private:
 
     void recompute_position()
     {
-        // recompute door position
+        // Recalculates door position
+
+        static uint32_t last_recompute_time = 0;
+        // store current time
+        const uint32_t now = millis();
+
+        if (this->current_operation != COVER_OPERATION_IDLE) // Door moving
+        {
+            float dir;
+            float action_dur;
+
+            // set dir and duration depending on current movement
+            if (this->current_operation == COVER_OPERATION_CLOSING)
+            {
+                // door closing
+                dir = -1.0f;
+                action_dur = this->close_duration;
+            }
+            else
+            {
+                // door opening
+                dir = 1.0f;
+                action_dur = this->open_duration;
+            }
+            // calculate position
+            float position = this->position;
+            position += dir * (now - last_recompute_time) / action_dur;
+            this->position = clamp(position, 0.0f, 1.0f);
+        }
+        // store time
+        last_recompute_time = now;
     }
 
     void push_button()
     {
-        // activate door switch
+        // Activates the door switch
+        // TODO add door switch call
+
+        // Cover state machine
+        if (this->current_operation == COVER_OPERATION_OPENING || this->current_operation == COVER_OPERATION_CLOSING)
+        {
+            // door moving
+            this->current_operation = COVER_OPERATION_IDLE;
+        }
+        else
+        {
+            // door idle
+            if (this->last_dir == COVER_OPERATION_OPENING)
+            {
+                // last action open
+                this->current_operation = COVER_OPERATION_CLOSING;
+                this->last_dir = COVER_OPERATION_CLOSING;
+            }
+            else
+            {
+                // last action close
+                this->current_operation = COVER_OPERATION_OPENING;
+                this->last_dir = COVER_OPERATION_OPENING;
+            }
+        }
     }
 };
