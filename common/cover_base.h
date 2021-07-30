3744fa588289670cba10ce967d0044435ac2f28b
Juan M <jmif@outlook.es>
2021-07-30 12:08:58 +0200

    Add Constructor for CustomGarageCover

--- common/cover_base.h
+++ common/cover_base.h
@@ -12,7 +12,19 @@ enum CoverTargetOperation : uint8_t
 class CustomGarageCover : public Component, public Cover
 {
 public:
-    // TODO add custom constructor
+    CoverOperation last_dir;               // last door direction (open/close)
+    CoverTargetOperation target_operation; // received action to execute
+
+    // Constructor
+    CustomGarageCover(Switch *door_switch, uint32_t switch_interval, uint32_t open_duration, uint32_t close_duration)
+    {
+        this->door_switch = door_switch;
+        this->last_dir = COVER_OPERATION_IDLE;
+        this->target_operation = TARGET_OPERATION_NONE;
+        this->switch_interval = switch_interval;
+        this->open_duration = open_duration;
+        this->close_duration = close_duration;
+    }
 
     void setup() override
     {
@@ -64,7 +76,7 @@ public:
         // recompute position every loop cycle
         this->recompute_position();
 
-        // perform one action if target action different than current operation
+        // perform one action if target operation different than current operation
         if (this->target_operation != TARGET_OPERATION_NONE && (static_cast<uint8_t>(this->target_operation) != static_cast<uint8_t>(this->current_operation)))
         {
             if (now - last_activation > switch_interval)
@@ -89,11 +101,10 @@ public:
     }
 
 private:
-    CoverOperation last_dir;                                       // last door direction (open/close)
-    CoverTargetOperation target_operation = TARGET_OPERATION_NONE; // received action to execute
-    uint32_t push_interval;                                        // time between switch activations
-    uint32_t open_duration;                                        // time the door needs to fully open
-    uint32_t close_duration;                                       // time the door needs to fully close
+    Switch *door_switch;     // switch that activates the door
+    uint32_t push_interval;  // time between switch activations
+    uint32_t open_duration;  // time the door needs to fully open
+    uint32_t close_duration; // time the door needs to fully close
 
     void recompute_position()
     {
@@ -135,6 +146,8 @@ private:
     {
         // Activates the door switch and update state
 
+        // activate switch
+        this->door_switch->turn_on();
         // cover state machine
         if (this->current_operation == COVER_OPERATION_OPENING || this->current_operation == COVER_OPERATION_CLOSING)
         {
@@ -157,5 +170,7 @@ private:
                 this->last_dir = COVER_OPERATION_OPENING;
             }
         }
+        // send current state
+        this->publish_state();
     }
 };
