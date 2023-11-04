2d75e2ad37ca26faaaf2da0d31c64972e08e1671
Juan M <jmif@outlook.es>
2023-11-04 18:31:50 +0100

    Add position control feature

--- common/cover_base.h
+++ common/cover_base.h
@@ -28,7 +28,8 @@ public:
         // initialize default values
         this->last_dir = COVER_OPERATION_IDLE;
         this->target_operation = TARGET_OPERATION_NONE;
-        // assume door is idle. If not once onr endstop is reached real state will be updated
+        this->target_position = 0.0f;
+        // assume door is idle. If not once one endstop is reached real state will be updated
         this->current_operation = COVER_OPERATION_IDLE;
         this->switch_activation_interval = 0;
         this->open_duration = 0;
@@ -65,6 +66,7 @@ public:
             // assume door its ad middle position
             this->position = 0.50f;
         }
+        this->target_position = this->position;
         // publish states
         this->publish_state(false);
     }
@@ -87,6 +89,8 @@ public:
         {
             // get requested position
             float pos = *call.get_position();
+            // ensure position is between 0 and 1
+            pos = clamp(pos, 0.0f, 1.0f);
 
             ESP_LOGD("CustomGarageCover", "Position command received: %0.2f", pos);
 
@@ -95,6 +99,7 @@ public:
                 // not at target
                 // calculate target operation
                 this->target_operation = pos < this->position ? TARGET_OPERATION_CLOSE : TARGET_OPERATION_OPEN;
+                this->target_position = pos;
             }
         }
         if (call.get_stop())
@@ -125,25 +130,47 @@ public:
         if (this->target_operation != TARGET_OPERATION_NONE && (static_cast<uint8_t>(this->target_operation) != static_cast<uint8_t>(this->current_operation)))
         {
             // only activate door if time greater than activation interval
-            if (now - last_activation > switch_activation_interval)
+            if ((now - last_activation) > this->switch_activation_interval)
             {
-                ESP_LOGD("CustomGarageCover", "Switch activated");
                 this->do_one_action();
                 last_activation = now;
                 last_publish_time = now;
 
                 // if door was commanded to activate once, delete target operation
-                if (target_operation == TARGET_OPERATION_ACTIVATE_ONCE)
+                if (this->target_operation == TARGET_OPERATION_ACTIVATE_ONCE)
                 {
-                    target_operation = TARGET_OPERATION_NONE;
+                    this->target_operation = TARGET_OPERATION_NONE;
                 }
             }
         }
+
+        // target reached, set target as None.
         else if (static_cast<uint8_t>(this->target_operation) == static_cast<uint8_t>(this->current_operation))
         {
-            // target reached, set target as None.
             ESP_LOGD("CustomGarageCover", "Target operation reached");
             this->target_operation = TARGET_OPERATION_NONE;
+        }
+
+        // when all operations are done and if door is not idling check if target position was reached
+        else if (this->target_operation == TARGET_OPERATION_NONE && this->current_operation != COVER_OPERATION_IDLE)
+        {
+            // do not stop door if position requested is FULL OPEN or FULL CLOSE
+            // in these cases door will be stopped by end_stop sensors
+            if (this->target_position != COVER_CLOSED && this->target_position != COVER_OPEN)
+            {
+                // check if target position was reached
+                if ((this->current_operation == COVER_OPERATION_OPENING && this->position >= this->target_position) ||
+                    (this->current_operation == COVER_OPERATION_CLOSING && this->position <= this->target_position))
+                {
+                    if ((now - last_activation) > this->switch_activation_interval)
+                    {
+                        ESP_LOGD("CustomGarageCover", "Target position: %0.2f - Stopped at %0.2f", this->target_position, this->position);
+                        this->do_one_action();
+                        last_activation = now;
+                        last_publish_time = now;
+                    }
+                }
+            }
         }
 
         // send current position every second
@@ -204,6 +231,7 @@ private:
     binary_sensor::BinarySensor *close_endstop_sensor; // binary sensor to detect when door is full closed
     cover::CoverOperation last_dir;                    // last door direction (open/close)
     CoverTargetOperation target_operation;             // received action to execute
+    float target_position;                             // target position to reach
     uint32_t switch_activation_interval;               // time between switch activations
     uint32_t open_duration;                            // time the door needs to fully open
     uint32_t close_duration;                           // time the door needs to fully close
@@ -249,6 +277,7 @@ private:
         // Activates the door switch and update state
 
         // activate switch
+        ESP_LOGD("CustomGarageCover", "Switch activated");
         this->door_switch->turn_on();
         // cover state machine
         if (this->current_operation == COVER_OPERATION_OPENING || this->current_operation == COVER_OPERATION_CLOSING)
